3c2559e27952a0299f5fbe0af9d1dbfe04e81c5b
p123abc <patpap002@gmail.com>
2018-10-11 15:27:48 -0400

    commit le routes donnait des bug a mon site web et a été remis a une version précédante

--- config/routes.php
+++ config/routes.php
@@ -52,12 +52,14 @@ Router::scope('/', function (RouteBuilder $routes) {
      * its action called 'display', and we pass a param to select the view file
      * to use (in this case, src/Template/Pages/home.ctp)...
      */
-    $routes->connect('/', ['controller' => 'Users', 'action' => 'login', 'login']);
+   //$routes->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
+    $routes->connect('/', ['controller' => 'Users', 'action' => 'index', 'user']);
 
     /**
      * ...and connect the rest of 'Pages' controller's URLs.
      */
-    $routes->connect('/users/*', ['controller' => 'Users', 'action' => 'login']);
+    //$routes->connect('/pages/*', ['controller' => 'Pages', 'action' => 'display']);
+    //$routes->connect('/users/*', ['controller' => 'Users', 'action' => 'index']);
 
     /**
      * Connect catchall routes for all controllers.
